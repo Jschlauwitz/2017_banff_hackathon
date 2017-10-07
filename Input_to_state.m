6b251aa7adb57151eefb6227dcb365017836dfb1
ASUS-PC\Justin <schlauwi@ualberta.ca>
2017-10-07 17:32:55 -0600

    Best data set yet

--- Input_to_state.m
+++ Input_to_state.m
@@ -4,26 +4,30 @@ classdef Input_to_state
     %   Detailed explanation goes here
     
     properties (Access = private)
-        UpperFreq
-        LwerFreq
+        Uppersam
+        Lwersam
         Cpipe
         FVal
         FMean
+        FLogical
+        FLhist
         FDev
         FTraceVal
     end
     
     methods
-        function obj = init(UFreq,LFreq, Trace)
+        function obj = init(Usam,Lsam, Trace)
             %INIT Construct an instance of this class
             %   Sets object parameters
-            obj.UpperFreq = UFreq;
-            obj.LwerFreq = LFreq;
-            obj.Cpipe = zeros(UFreq);
-            obj.FMean = zeros(UFreq-LFreq);
-            obj.FVal = zeros(UFreq-LwerFreq);
-            obj.FDev = zeros(UFreq-LwerFreq);
-            obj.FTraceVal = ones(UFreq-LwerFreq)*Trace;
+            obj.Uppersam = Usam;
+            obj.Lwersam = Lsam;
+            obj.Cpipe = zeros(Usam);
+            obj.FMean = zeros(Usam-Lsam);
+            obj.FVal = zeros(Usam-Lsam);
+            obj.FDev = zeros(Usam-Lsam);
+            obj.FLogical = zeros(Usam-Lsam);
+            obj.FLhist = zeros(Usam-Lsam);            
+            obj.FTraceVal = ones(Usam-Lsam)*Trace;
         end
         
         function obj = push(obj, InValue)
@@ -33,13 +37,27 @@ classdef Input_to_state
             obj.Cpipe(1) = InValue;
         end
         function obj = freqs(obj)
-            %FREQS Convert to frequency domain and find mean and avg
-            %frequencies
+            %FREQS Convert to frequency domain and find mean and Std
+            %deviation
             %   Detailed explanation goes here
-            obj.FVal = fft(obj.Cpipe);
+            temp = fft(obj.Cpipe);
+            obj.FVal = temp(obj.Lwersam:end);
             obj.FMean = obj.FMean.*(1-obj.FTraceVal) + obj.FVal.*obj.FTraceVal;
             obj.FDev = obj.FDev.*(1-obj.FTraceVal) + abs(obj.FVal-obj.FMean).*obj.FTraceVal;
         end
+        function obj = freqs_to_logic(obj)
+            %FREQS Convert frequencies to a logical range
+            %   Detailed explanation goes here
+            obj.FLogical = Sig((obj.FVal-obj.FMean)./obj.FDev);
+            obj.FLhist = obj.FLhist.*(1-obj.FTraceVal) + obj.FLogical.*obj.FTraceVal;
+        end
+        function output = Sig(input)
+            %FREQS Convert frequencies to a logical range
+            %   Detailed explanation goes here
+            output = sign(input).*abs(input)./(abs(input)+1);
+        end
+        
+        
         function val = get.FVal(obj)
             %METHOD2 sSummary of this method goes here
             %   Detailed explanation goes here
