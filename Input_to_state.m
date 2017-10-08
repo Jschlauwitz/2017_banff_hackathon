8f02812904406c705fadbc5a24cff11318a09082
ASUS-PC\Justin <schlauwi@ualberta.ca>
2017-10-07 19:16:25 -0600

    Input_to_state and test_run added

--- Input_to_state.m
+++ Input_to_state.m
@@ -1,4 +1,4 @@
-classdef Input_to_state
+classdef Input_to_state < handle
     %Input_to_state Converts received frequency and trace to right/left
     %state
     %   Detailed explanation goes here
@@ -16,18 +16,18 @@ classdef Input_to_state
     end
     
     methods
-        function obj = init(Usam,Lsam, Trace)
+        function obj = init(obj, Usam, Lsam, Trace)
             %INIT Construct an instance of this class
             %   Sets object parameters
             obj.Uppersam = Usam;
             obj.Lwersam = Lsam;
-            obj.Cpipe = zeros(Usam);
-            obj.FMean = zeros(Usam-Lsam);
-            obj.FVal = zeros(Usam-Lsam);
-            obj.FDev = zeros(Usam-Lsam);
-            obj.FLogical = zeros(Usam-Lsam);
-            obj.FLhist = zeros(Usam-Lsam);            
-            obj.FTraceVal = ones(Usam-Lsam)*Trace;
+            obj.Cpipe = zeros(Lsam,1);
+            obj.FMean = zeros(Lsam-Usam,1);
+            obj.FVal = zeros(Lsam-Usam,1);
+            obj.FDev = zeros(Lsam-Usam,1);
+            obj.FLogical = zeros(Lsam-Usam,1);
+            obj.FLhist = zeros(Lsam-Usam,1);            
+            obj.FTraceVal = ones(Lsam-Usam,1)*Trace;
         end
         
         function obj = push(obj, InValue)
@@ -41,35 +41,51 @@ classdef Input_to_state
             %deviation
             %   Detailed explanation goes here
             temp = fft(obj.Cpipe);
-            obj.FVal = temp(obj.Lwersam:end);
-            obj.FMean = obj.FMean.*(1-obj.FTraceVal) + obj.FVal.*obj.FTraceVal;
-            obj.FDev = obj.FDev.*(1-obj.FTraceVal) + abs(obj.FVal-obj.FMean).*obj.FTraceVal;
+            obj.FVal = temp((obj.Uppersam+1):end);
+            for i= 1:length(obj.FVal)
+                obj.FMean(i) = obj.FMean(i).*(1-obj.FTraceVal(i)) + obj.FVal(i).*obj.FTraceVal(i);
+                obj.FDev(i) = obj.FDev(i).*(1-obj.FTraceVal(i)) + abs(obj.FVal(i)-obj.FMean(i)).*obj.FTraceVal(i);
+            end
         end
         function obj = freqs_to_logic(obj)
             %FREQS Convert frequencies to a logical range
             %   Detailed explanation goes here
-            obj.FLogical = Sig((obj.FVal-obj.FMean)./obj.FDev);
-            obj.FLhist = obj.FLhist.*(1-obj.FTraceVal) + obj.FLogical.*obj.FTraceVal;
+            for i= 1:length(obj.FVal)
+            obj.FLogical(i) = Sig((obj.FVal(i)-obj.FMean(i))./obj.FDev(i));
+            obj.FLhist(i) = obj.FLhist(i).*(1-obj.FTraceVal(i)) + obj.FLogical(i).*obj.FTraceVal(i);
+            end
         end
+        function output = Crun(obj, input)
+            %FREQS Convert frequencies to a logical range
+            %   Detailed explanation goes here
+            obj.push(input);
+            obj.freqs();
+            obj.freqs_to_logic();
+            output = obj.FLogical;
+        end
+        
+        function val = get.FVal(obj)
+            %METHOD2 sSummary of this method goes here
+            %   Detailed explanation goes here
+            val = obj.FVal;
+        end
+        function val = get.FMean(obj)
+            %METHOD2 sSummary of this method goes here
+            %   Detailed explanation goes here
+            val = obj.FMean;
+        end
+        function val = get.FDev(obj)
+            %METHOD2 sSummary of this method goes here
+            %   Detailed explanation goes here
+            val = obj.FDev;
+        end
+    end
+end
+
+
         function output = Sig(input)
             %FREQS Convert frequencies to a logical range
             %   Detailed explanation goes here
             output = sign(input).*abs(input)./(abs(input)+1);
         end
         
-        
-        function val = get.FVal(obj)
-            %METHOD2 sSummary of this method goes here
-            %   Detailed explanation goes here
-        end
-        function val = get.FMean(obj)
-            %METHOD2 sSummary of this method goes here
-            %   Detailed explanation goes here
-        end
-        function val = get.FDev(obj)
-            %METHOD2 sSummary of this method goes here
-            %   Detailed explanation goes here
-        end
-    end
-end
-
